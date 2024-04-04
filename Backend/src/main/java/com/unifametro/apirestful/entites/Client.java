61cfb51dfac4919682aefb26abc1e06fb2c1b3c4
RafaelSales93 <fco.rsales@gmail.com>
2024-04-03 21:26:17 -0300

    ClientService ClientRepository

--- Backend/src/main/java/com/unifametro/apirestful/entites/Client.java
+++ Backend/src/main/java/com/unifametro/apirestful/entites/Client.java
@@ -4,12 +4,28 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name ="tb_client")
+
+
 public class Client implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String cpf;
+	
+	@Column(columnDefinition = "TIMESTAMP WITH TIME ZONE")
 	private Instant birthDate;
 	private Double income;
 	
